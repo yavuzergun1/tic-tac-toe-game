6c2cdfd2e06d9d0eaad78863bd13dd00aa83013d
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-08-25 22:59:55 +0300

    alert styling

--- src/Components/Draw.js
+++ src/Components/Draw.js
@@ -1,27 +1,27 @@
-import * as React from 'react';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import drawAudio from "../assets/draw.wav";
 
-export default function Draw({initialSquares, setSquares}) {
+export default function Draw({ initialSquares, setSquares }) {
   const playDrawAudio = () => {
     new Audio(drawAudio).play();
   };
-  playDrawAudio()
-    const handleClick = () =>{
-        setSquares(initialSquares)
-
-    }
+  playDrawAudio();
+  const handleClick = () => {
+    setSquares(initialSquares);
+  };
   return (
-    <Stack sx={{ width: '100%' }} spacing={2} >
-      <Alert severity="info">
-  <AlertTitle>Draw</AlertTitle>
-  There is no Winner — <strong>Try Again!</strong> <br />
-  <Button onClick={handleClick}>Replay</Button>
-</Alert>
-    </Stack>
+    <div className="alertButton">
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <Alert variant="filled" severity="info">
+          <AlertTitle>Draw!</AlertTitle>
+           <strong>There is no Winner... </strong> <br />
+          <Button onClick={handleClick}>Replay</Button>
+        </Alert>
+      </Stack>
+    </div>
   );
 }
-
